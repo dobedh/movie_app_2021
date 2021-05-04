cfd14fcb7c949c7aa4be87fffcfaf0957c3136c8
dobedh <dobedh@gmail.com>
2021-05-04 23:49:34 +0900

    axios, ES6 and etc

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 3000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div> {isLoading ? "Loading .." : "I'm ready!"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading .." : "I'm ready!"}</div>;
   }
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes = "prop-types"
+
+function Movie () {
+    
+}
+
+export default Movie
